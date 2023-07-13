9737fa1b01025ad1ae71aa674949c76c41ad6f01
Viktoriia-Parpalii <127688031+Viktoriia-Parpalii@users.noreply.github.com>
2023-07-13 18:03:18 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,19 @@
 const inputEl = document.getElementById("name-input");
 const nameLabel = document.getElementById("name-output");
 
-inputEl.addEventListener("input", onImputChange);
+// inputEl.addEventListener("input", changeName);
 
-function onImputChange(event) {
+// function changeName(event) {
+//   if (event) nameLabel.textContent = event.currentTarget.value;
+
+// }
+
+inputEl.addEventListener("input", changeName);
+inputEl.addEventListener("blur", staticName);
+
+function changeName(event) {
   nameLabel.textContent = event.currentTarget.value;
+}
+function staticName(event) {
+  if (nameLabel.textContent === "") nameLabel.textContent = "Anonymous";
 }
