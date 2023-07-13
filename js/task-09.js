f344d5396eeab77ba646b7510af13a997691ce3c
Viktoriia-Parpalii <127688031+Viktoriia-Parpalii@users.noreply.github.com>
2023-07-13 18:50:18 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,14 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorEl = document.querySelector(".color");
+const btnEl = document.querySelector(".change-color");
+const bodyEl = document.querySelector("body");
+
+btnEl.addEventListener("click", changeColor);
+
+function changeColor() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = getRandomHexColor();
+}
