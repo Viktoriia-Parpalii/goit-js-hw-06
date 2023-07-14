0136c6582dc8fde5f33da04019027cf9d12096d7
Viktoriia-Parpalii <127688031+Viktoriia-Parpalii@users.noreply.github.com>
2023-07-14 21:24:14 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,34 +1,34 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
 
-const refs = {
-  controls: document.getElementById("controls"),
-  input: controls.firstElementChild,
-  createBtn: controls.querySelector("[data-create]"),
-  destroyBtn: controls.querySelector("[data-destroy]"),
-  boxes: document.getElementById("boxes"),
-};
+// const refs = {
+//   controls: document.getElementById("controls"),
+//   input: controls.firstElementChild,
+//   createBtn: controls.querySelector("[data-create]"),
+//   destroyBtn: controls.querySelector("[data-destroy]"),
+//   boxes: document.getElementById("boxes"),
+// };
 
-refs.createBtn.addEventListener("click", onClick);
+// refs.createBtn.addEventListener("click", onClick);
 
-function onClick() {
-  createBoxes();
-}
-function createBoxes(amount) {
-  const createDiv = document.createElement("div");
-  createDiv.style.width = 30 + "px";
-  createDiv.style.height = 30 + "px";
+// function onClick() {
+//   createBoxes();
+// }
+// function createBoxes(amount) {
+//   const createDiv = document.createElement("div");
+//   createDiv.style.width = 30 + "px";
+//   createDiv.style.height = 30 + "px";
 
-  let array = [];
-  array.push(createDiv);
+//   let array = [];
+//   array.push(createDiv);
 
-  refs.boxes.append(...array);
-}
+//   refs.boxes.append(...array);
+// }
 
-refs.input.addEventListener("input", onInput);
-function onInput(event) {
-  const amount = event.elements.currentTarget.value;
-}
+// refs.input.addEventListener("input", onInput);
+// function onInput(event) {
+//   const amount = event.elements.currentTarget.value;
+// }
