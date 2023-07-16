8a966856ebf5266de1ba7203b06d7a9314279a39
Viktoriia-Parpalii <127688031+Viktoriia-Parpalii@users.noreply.github.com>
2023-07-16 12:47:22 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,34 +1,51 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
-// const refs = {
-//   controls: document.getElementById("controls"),
-//   input: controls.firstElementChild,
-//   createBtn: controls.querySelector("[data-create]"),
-//   destroyBtn: controls.querySelector("[data-destroy]"),
-//   boxes: document.getElementById("boxes"),
-// };
+const refs = {
+  controls: document.getElementById("controls"),
+  input: controls.firstElementChild,
+  createBtn: controls.querySelector("[data-create]"),
+  destroyBtn: controls.querySelector("[data-destroy]"),
+  boxes: document.getElementById("boxes"),
+};
+refs.boxes.style.display = "flex";
+refs.boxes.style.flexDirection = "column";
+refs.boxes.style.gap = 20 + "px";
+refs.boxes.style.justifyContent = "center";
+refs.boxes.style.alignItems = "center";
+refs.boxes.style.marginTop = 20 + "px";
 
-// refs.createBtn.addEventListener("click", onClick);
+refs.createBtn.addEventListener("click", onClick);
 
-// function onClick() {
-//   createBoxes();
-// }
-// function createBoxes(amount) {
-//   const createDiv = document.createElement("div");
-//   createDiv.style.width = 30 + "px";
-//   createDiv.style.height = 30 + "px";
+function onClick() {
+  const inputValue = Number(refs.input.value);
+  refs.boxes.innerHTML = "";
+  createBoxes(inputValue);
+}
 
-//   let array = [];
-//   array.push(createDiv);
+function createBoxes(amount) {
+  let arrayBoxes = [];
+  let boxSize = 30;
 
-//   refs.boxes.append(...array);
-// }
+  for (let i = 0; i < amount; i += 1) {
+    boxSize += 10;
+    const createDiv = document.createElement("div");
+    createDiv.style.width = boxSize + "px";
+    createDiv.style.height = boxSize + "px";
+    createDiv.style.borderRadius = 20 + "px";
+    createDiv.style.boxShadow = "5px 5px 15px 5px #000000";
 
-// refs.input.addEventListener("input", onInput);
-// function onInput(event) {
-//   const amount = event.elements.currentTarget.value;
-// }
+    createDiv.style.backgroundColor = getRandomHexColor();
+    arrayBoxes.push(createDiv);
+  }
+  refs.boxes.append(...arrayBoxes);
+}
+
+refs.destroyBtn.addEventListener("click", destroyBoxes);
+function destroyBoxes() {
+  refs.boxes.innerHTML = "";
+  refs.input.value = "";
+}
